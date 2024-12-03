901b2b45cf8b7ae7ebd302b4023434eaa19accef
xcfox <xcfox@qq.com>
2024-12-03 18:31:40 +0800

    test(define-entity): update tests to cover entity definition

--- tests/features/define-entity.test.ts
+++ tests/features/define-entity.test.ts
@@ -1,8 +1,8 @@
-import { EntitySchema, InferEntity, Reference, Collection, InferEntityFromProperties, RequiredEntityData, Opt, Ref, m, TextType } from '@mikro-orm/core';
+import { EntitySchema, InferEntity, Reference, Collection, InferEntityFromProperties, RequiredEntityData, Opt, Ref, m, TextType, types, ReferenceKind } from '@mikro-orm/core';
 import { MikroORM } from '@mikro-orm/sqlite';
 import { IsExact, assert } from 'conditional-type-checks';
 
-describe('InferEntity', () => {
+describe('define-entity', () => {
   const Bar = EntitySchema.define({
     name:'bar',
     properties: t => ({
@@ -12,7 +12,7 @@ describe('InferEntity', () => {
 
   interface IBar extends InferEntity<typeof Bar> {}
 
-  it('should infer properties', () => {
+  it('should define entity with properties', () => {
     const Foo = EntitySchema.define({
       name:'foo',
       properties: {
@@ -44,9 +44,27 @@ describe('InferEntity', () => {
 
     type IFoo = InferEntity<typeof Foo>;
     assert<IsExact<IFoo, IFooExpected>>(true);
+
+    const FooExpected = new EntitySchema({
+      name: 'foo',
+      properties: {
+        string:{ type: types.string },
+        number:{ type: types.float },
+        date:{ type: types.datetime },
+        array:{ type: types.array },
+        enum:{ enum: true, items:['a', 'b'] },
+        json: { type: types.json },
+        text:{ type: TextType },
+        text1:{ type: 'text' },
+        float:{ type: 'float', onCreate: expect.any(Function) },
+        uuid:{ type: types.uuid },
+      },
+    });
+
+    expect(Foo.meta.properties).toEqual(FooExpected.meta.properties);
   });
 
-  it('should infer properties from combination', () => {
+  it('should define entity with properties from combination', () => {
     const WithTimes = EntitySchema.define({
       name:'WithTimes',
       properties: {
@@ -71,9 +89,20 @@ describe('InferEntity', () => {
 
     type IFoo = InferEntity<typeof Foo>;
     assert<IsExact<IFoo, IFooExpected>>(true);
+
+    const FooExpected = new EntitySchema<IFooExpected>({
+      name: 'foo',
+      properties: {
+        createdAt:{ type: types.datetime },
+        updatedAt:{ type: types.datetime },
+        bar:{ type: types.string },
+      },
+    });
+
+    expect(Foo.meta.properties).toEqual(FooExpected.meta.properties);
   });
 
-  it('should infer nullable properties', () => {
+  it('should define entity with nullable properties', () => {
     const Foo = EntitySchema.define({
       name:'foo',
       properties: {
@@ -99,9 +128,24 @@ describe('InferEntity', () => {
 
     type IFoo = InferEntity<typeof Foo>;
     assert<IsExact<IFoo, IFooExpected>>(true);
+
+    const FooExpected = new EntitySchema<IFooExpected>({
+      name: 'foo',
+      properties: {
+        directly:{ type: types.text },
+        required:{ type: types.text, nullable: false },
+        nullable:{ type: types.text, nullable: true },
+        json:{ type: types.json },
+        jsonRequired:{ type: types.json, nullable: false },
+        jsonOptional:{ type: types.json, onCreate: expect.any(Function) },
+        jsonNullable:{ type: types.json, nullable: true },
+      },
+    });
+
+    expect(Foo.meta.properties).toEqual(FooExpected.meta.properties);
   });
 
-  it('should infer manyToOne relations', () => {
+  it('should define entity with manyToOne relations', () => {
     const Foo = EntitySchema.define({
       name:'foo',
       properties: {
@@ -121,9 +165,21 @@ describe('InferEntity', () => {
 
     type IFoo = InferEntity<typeof Foo>;
     assert<IsExact<IFoo, IFooExpected>>(true);
+
+    const FooExpected = new EntitySchema<IFooExpected>({
+      name: 'foo',
+      properties: {
+        directly:{ kind: ReferenceKind.MANY_TO_ONE, entity: expect.any(Function) },
+        ref:{ kind: ReferenceKind.MANY_TO_ONE, entity: expect.any(Function), ref: true },
+        nullableDirectly:{ kind: ReferenceKind.MANY_TO_ONE, entity: expect.any(Function), nullable: true },
+        nullableRef:{ kind: ReferenceKind.MANY_TO_ONE, entity: expect.any(Function), ref:true, nullable: true },
+      },
+    });
+
+    expect(Foo.meta.properties).toEqual(FooExpected.meta.properties);
   });
 
-  it('should infer oneToOne relations', () => {
+  it('should define entity with oneToOne relations', () => {
     const Foo = EntitySchema.define({
       name:'foo',
       properties: {
@@ -143,9 +199,21 @@ describe('InferEntity', () => {
 
     type IFoo = InferEntity<typeof Foo>;
     assert<IsExact<IFoo, IFooExpected>>(true);
+
+    const FooExpected = new EntitySchema<IFooExpected>({
+      name: 'foo',
+      properties: {
+        directly: { kind: ReferenceKind.ONE_TO_ONE, entity: expect.any(Function) },
+        ref: { kind: ReferenceKind.ONE_TO_ONE, entity: expect.any(Function), ref: true },
+        nullableDirectly: { kind: ReferenceKind.ONE_TO_ONE, entity: expect.any(Function), nullable: true },
+        nullableRef: { kind: ReferenceKind.ONE_TO_ONE, entity: expect.any(Function), ref:true, nullable: true },
+      },
+    });
+
+    expect(Foo.meta.properties).toEqual(FooExpected.meta.properties);
   });
 
-  it('should infer oneToMany relations', () => {
+  it('should define entity with oneToMany relations', () => {
     const Foo = EntitySchema.define({
       name:'foo',
       properties: {
@@ -161,9 +229,19 @@ describe('InferEntity', () => {
 
     type IFoo = InferEntity<typeof Foo>;
     assert<IsExact<IFoo, IFooExpected>>(true);
+
+    const FooExpected = new EntitySchema<IFooExpected>({
+      name: 'foo',
+      properties: {
+        directly: { kind: ReferenceKind.ONE_TO_MANY, entity: expect.any(Function), mappedBy : 'foo' },
+        nullableDirectly: { kind: ReferenceKind.ONE_TO_MANY, entity: expect.any(Function), mappedBy : 'foo', nullable: true },
+      },
+    });
+
+    expect(Foo.meta.properties).toEqual(FooExpected.meta.properties);
   });
 
-  it('should infer manyToMany relations', () => {
+  it('should define entity with manyToMany relations', () => {
     const Foo = EntitySchema.define({
       name:'foo',
       properties: {
@@ -179,9 +257,19 @@ describe('InferEntity', () => {
 
     type IFoo = InferEntity<typeof Foo>;
     assert<IsExact<IFoo, IFooExpected>>(true);
+
+    const FooExpected = new EntitySchema<IFooExpected>({
+      name: 'foo',
+      properties: {
+        directly: { kind: ReferenceKind.MANY_TO_MANY, entity: expect.any(Function), mappedBy : 'foo' },
+        nullableDirectly: { kind: ReferenceKind.MANY_TO_MANY, entity: expect.any(Function), mappedBy : 'foo', nullable: true },
+      },
+    });
+
+    expect(Foo.meta.properties).toEqual(FooExpected.meta.properties);
   });
 
-  it('should infer embedded properties', () => {
+  it('should define entity with embedded properties', () => {
     const Foo = EntitySchema.define({
       name:'foo',
       properties: {
@@ -201,9 +289,21 @@ describe('InferEntity', () => {
 
     type IFoo = InferEntity<typeof Foo>;
     assert<IsExact<IFoo, IFooExpected>>(true);
+
+    const FooExpected = new EntitySchema<IFooExpected>({
+      name: 'foo',
+      properties: {
+        directly:{ kind: ReferenceKind.EMBEDDED, entity: expect.any(Function) },
+        ref:{ kind: ReferenceKind.EMBEDDED, entity: expect.any(Function), ref: true },
+        nullableDirectly:{ kind: ReferenceKind.EMBEDDED, entity: expect.any(Function), nullable: true },
+        nullableRef:{ kind: ReferenceKind.EMBEDDED, entity: expect.any(Function), ref: true, nullable: true },
+      },
+    });
+
+    expect(Foo.meta.properties).toEqual(FooExpected.meta.properties);
   });
 
-  it('should infer enum properties', () => {
+  it('should define entity with enum properties', () => {
     enum Baz {
       A,
       B,
@@ -243,6 +343,25 @@ describe('InferEntity', () => {
 
     type IFoo = InferEntity<typeof Foo>;
     assert<IsExact<IFoo, IFooExpected>>(true);
+
+    const FooExpected = new EntitySchema<IFooExpected>({
+      name: 'foo',
+      properties: {
+        items: { enum: true, items: ab },
+        arrayItems: { enum: true, items: ab, array: true },
+        refItems: { enum: true, items: ab, ref: true },
+        nullableItems: { enum: true, items: ab, nullable: true },
+        nullableRefItems: { enum: true, items: ab, ref: true, nullable: true },
+
+        enum: { enum: true, items: expect.any(Function) },
+        enumArray: { enum: true, items: expect.any(Function), array: true },
+        enumRef: { enum: true, items: expect.any(Function), ref: true },
+        nullableEnum: { enum: true, items: expect.any(Function), nullable: true },
+        nullableEnumRef: { enum: true, items: expect.any(Function), ref: true, nullable: true },
+      },
+    });
+
+    expect(Foo.meta.properties).toEqual(FooExpected.meta.properties);
   });
 
   it('should infer properties for circular reference entity', () => {
@@ -299,9 +418,6 @@ describe('InferEntity', () => {
     assert<IsExact<RequiredFoo, RequiredFooExpected>>(true);
   });
 
-});
-
-describe('define-entity', () => {
   const WithId = m.defineProperties({
     id: m.integer({ primary: true }),
   });
