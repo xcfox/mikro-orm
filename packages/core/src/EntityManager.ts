b542d749691cb281ada181b289d7caf217434d92
Martin Adámek <banan23@gmail.com>
2025-02-08 11:42:46 +0100

    feat(sql): replace knex with kysely (#6400)
    Closes #5646

--- packages/core/src/EntityManager.ts
+++ packages/core/src/EntityManager.ts
@@ -1312,7 +1312,7 @@ export class EntityManager<Driver extends IDatabaseDriver = IDatabaseDriver> {
         }
 
         return ret;
-      }, { ...options, eventBroadcaster: new TransactionEventBroadcaster(fork, undefined, { topLevelTransaction: !options.ctx }) });
+      }, { ...options, eventBroadcaster: new TransactionEventBroadcaster(fork, { topLevelTransaction: !options.ctx }) });
     });
   }
 
@@ -1327,7 +1327,7 @@ export class EntityManager<Driver extends IDatabaseDriver = IDatabaseDriver> {
     const em = this.getContext(false);
     em.transactionContext = await em.getConnection('write').begin({
       ...options,
-      eventBroadcaster: new TransactionEventBroadcaster(em, undefined, { topLevelTransaction: !options.ctx }),
+      eventBroadcaster: new TransactionEventBroadcaster(em, { topLevelTransaction: !options.ctx }),
     });
   }
 
