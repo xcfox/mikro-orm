b4f625dd7be4c2545d0f13706b1c38db9f1f96c4
Martin Adámek <banan23@gmail.com>
2025-04-14 22:12:13 +0200

    test: fix tests after rebase

--- packages/knex/src/dialects/mssql/MsSqlNativeQueryBuilder.ts
+++ packages/knex/src/dialects/mssql/MsSqlNativeQueryBuilder.ts
@@ -90,7 +90,9 @@ export class MsSqlNativeQueryBuilder extends NativeQueryBuilder {
       this.parts.push('then update set');
 
       if (!clause.merge || Array.isArray(clause.merge)) {
-        const parts = keys.map((column: any) => `${this.quote(column)} = tsource.${this.quote(column)}`);
+        const parts = keys
+          .filter(field => !Array.isArray(clause.fields) || !clause.fields.includes(field))
+          .map((column: any) => `${this.quote(column)} = tsource.${this.quote(column)}`);
         this.parts.push(parts.join(', '));
       } else if (typeof clause.merge === 'object') {
         const parts = Object.entries(clause.merge).map(([key, value]) => {
